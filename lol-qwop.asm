14cf08a3479e57b2efc38f048f8e64e22ccabf27
ix <arcetera@openmailbox.org>
2016-06-22 18:17:24 -0400

    add 360 assembly

---
+++ lol-qwop.asm
@@ -0,0 +1,8 @@
+        LA    1,MSGAREA
+        SVC   35
+        BR    14
+MSGAREA EQU   *
+        DC    AL2(19)
+        DC    XL2'00'
+        DC    C'lol-qwop'
+        END
---
+++ lol-qwop.gs
@@ -0,0 +1,3 @@
+function lolQwop() {
+     Logger.log('lol-qwop');
+ }
